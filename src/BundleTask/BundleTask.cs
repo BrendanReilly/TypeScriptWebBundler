ba2664cfbe05836319f29000f3bac03ba7b7dd6a
Brendan Reilly <brendan.benting@gmail.com>
2019-07-12 01:03:40 -0400

    Added BundleTask project Added lib folder for resources

---
+++ src/BundleTask/BundleTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Resources;
+using Microsoft.Build.Utilities;
+
+namespace BReilly
+{
+    public class BundleTask : Task
+    {
+
+
+        public override bool Execute()
+        {
+            Console.WriteLine("Task executed");
+            throw new NotImplementedException();
+        }
+    }
+}
